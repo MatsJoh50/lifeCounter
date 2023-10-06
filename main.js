4ade6abef2ba49ff1d9f6c9b011008132ea9531d
Mats Johansson <matjoh50@gmail.com>
2023-10-06 11:18:53 +0200

    Fully functional

--- main.js
+++ main.js
@@ -5,30 +5,108 @@ let cmdDmgTopRight = 0;
 let cmdDmgBottomLeft = 0;
 let cmdDmgBottomRight = 0;
 
-document.querySelector("#tLeftUp").addEventListener("click", function(){
+document.querySelector("#tLeft").innerText = cmdDmgTopLeft;
+document.querySelector("#tRight").innerText = cmdDmgTopRight;
+document.querySelector("#bLeft").innerText = cmdDmgBottomLeft;
+document.querySelector("#bRight").innerText = cmdDmgBottomRight;
+document.querySelector("#bRight").innerText = cmdDmgBottomRight;
+document.querySelector("#totalLife").innerText = totalLife;
+
+
+
+///////// TOP LEFT \\\\\\\\\\
+document.querySelector("#tLeftUp").addEventListener("click", function () {
     cmdDmgTopLeft = cmdDmgTopLeft + 1;
     document.querySelector("#tLeft").innerText = cmdDmgTopLeft;
 
 });
+document.querySelector("#tLeftDown").addEventListener("click", function () {
+    cmdDmgTopLeft = cmdDmgTopLeft - 1;
+    document.querySelector("#tLeft").innerText = cmdDmgTopLeft;
 
-function addToNumber(possition, value){
-    switch(possition){
+});
+///////// TOP RIGHT \\\\\\\\\\
+document.querySelector("#tRightUp").addEventListener("click", function () {
+    cmdDmgTopRight = cmdDmgTopRight + 1;
+    document.querySelector("#tRight").innerText = cmdDmgTopRight;
+
+});
+document.querySelector("#tRightDown").addEventListener("click", function () {
+    cmdDmgTopRight = cmdDmgTopRight - 1;
+    document.querySelector("#tRight").innerText = cmdDmgTopRight;
+
+});
+///////// BOTTOM LEFT \\\\\\\\\\
+document.querySelector("#bLeftUp").addEventListener("click", function () {
+    cmdDmgBottomLeft = cmdDmgBottomLeft + 1;
+    document.querySelector("#bLeft").innerText = cmdDmgBottomLeft;
+
+});
+document.querySelector("#bLeftDown").addEventListener("click", function () {
+    cmdDmgBottomLeft = cmdDmgBottomLeft - 1;
+    document.querySelector("#bLeft").innerText = cmdDmgBottomLeft;
+
+});
+///////// BOTTOM RIGHT \\\\\\\\\\
+document.querySelector("#bRightUp").addEventListener("click", function () {
+    cmdDmgBottomRight = cmdDmgBottomRight + 1;
+    document.querySelector("#bRight").innerText = cmdDmgBottomRight;
+
+});
+document.querySelector("#bRightDown").addEventListener("click", function () {
+    cmdDmgBottomRight = cmdDmgBottomRight - 1;
+    document.querySelector("#bRight").innerText = cmdDmgBottomRight;
+
+});
+
+
+///// LIFE TOTAL \\\\\\
+document.querySelector("#oneUp").addEventListener("click", function () {
+    totalLife = totalLife + 1;
+    document.querySelector("#totalLife").innerText = totalLife;
+
+});
+document.querySelector("#fiveUp").addEventListener("click", function () {
+    totalLife = totalLife + 5;
+    document.querySelector("#totalLife").innerText = totalLife;
+
+});
+document.querySelector("#oneDown").addEventListener("click", function () {
+    totalLife = totalLife - 1;
+    document.querySelector("#totalLife").innerText = totalLife;
+
+});
+document.querySelector("#fiveDown").addEventListener("click", function () {
+    totalLife = totalLife - 5;
+    document.querySelector("#totalLife").innerText = totalLife;
+
+});
+
+
+
+function addToNumber(possition, value) {
+    switch (possition) {
         case tLeft:
             cmdDmgTopLeft = cmdDmgTopLeft + value;
             // rightPossition.innerText = cmdDmgTopLeft;
+
+            document.querySelector(`#${possition}`).innerHTML = "";
             document.querySelector(`#${possition}`).innerHTML = cmdDmgTopLeft;
             break;
         case tRight:
             cmdDmgTopRight = cmdDmgTopRight + value;
-            rightPossition.innerText(cmdDmgTopRight);
+            document.querySelector(`#${possition}`).innerHTML = "";
+            document.querySelector(`#${possition}`).innerHTML = cmdDmgTopRight;
             break;
         case bLeft:
             cmdDmgBottomLeft = cmdDmgBottomLeft + value;
-            rightPossition.innerText(cmdDmgBottomLeft);
+            document.querySelector(`#${possition}`).innerHTML = "";
+            document.querySelector(`#${possition}`).innerHTML = cmdDmgBottomLeft;
             break;
         case bRight:
             possition = possition + value;
-            rightPossition.innerText(possition);
+            document.querySelector(`#${possition}`).innerHTML = "";
+            document.querySelector(`#${possition}`).innerHTML = cmdDmgBottomRight;
             break;
 
     }
